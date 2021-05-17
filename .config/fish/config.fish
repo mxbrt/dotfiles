2f09fcccd0c9e92d5db461e33d74d92c3872cb2b
Max Breitenfeldt <git@mxbr.me>
2021-05-17 22:42:26 +0200

    Add fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,15 @@
+# If you come from bash you might have to change your $PATH.
+export PATH="$HOME/bin:$HOME/.local/bin:$PATH"
+
+# User configuration
+export EDITOR=nvim
+export XDG_CURRENT_DESKTOP=sway
+export LIBVA_DRIVER_NAME=radeon
+
+alias vim=nvim
+alias cat=bat
+alias ls=exa
+alias zathura='zathura --fork'
+alias dotfiles='git --git-dir=$HOME/.gitdotfiles/ --work-tree=$HOME'
+alias lg=lazygit
+
